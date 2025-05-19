9d1ddf435429d322cc28a88f4f9ec10ecb63c5bb
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-05-19 11:36:47 +0100

    examenRest

---
+++ examen/src/main/java/com/example/examen/web/ClientRestController.java
@@ -0,0 +1,37 @@
+package com.example.examen.web;
+
+import com.example.examen.dtos.ClientDTO;
+import com.example.examen.services.CreditService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class ClientRestController {
+    private CreditService creditService;
+    @GetMapping("/clients")
+
+    public List<ClientDTO> clients(){
+        return creditService.getAllClients();
+    }
+
+    @GetMapping("/customers/{id}")
+
+    public ClientDTO getClient( @PathVariable(name = "id") Long Id) {
+        return  creditService.getClientById(Id);
+    }
+
+    @PostMapping("/customers")
+
+
+    public ClientDTO saveClient(@RequestBody ClientDTO clientDTO )  {
+        return  creditService.saveClient(clientDTO);
+    }
+    @DeleteMapping ("/customers/{customerId}")
+
+    public void deletClient(@PathVariable Long Id){
+        creditService.deleteClient(Id);
+    }
+}
